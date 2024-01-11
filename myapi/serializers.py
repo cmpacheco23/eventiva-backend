365afa0390b60740055a63cc25d473e5080ed54c
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-11 18:26:23 -0500

    add serializers

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from .models import Poll, Event
+
+class EventSerializer(serializers.ModelSerializer):
+  polls = 'PollSerializer'
 
 class PollSerializer(serializers.ModelSerializer):
   class Meta:
     model = Poll 
     fields = ['option1', 'option2', 'option3', 'option4', 'option5', 'option6']
-    
