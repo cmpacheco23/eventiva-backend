7a26a34408bb7514aa21683b791957f2d806114b
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-11 17:47:32 -0500

    fix model errors

--- myapi/models.py
+++ myapi/models.py
@@ -9,15 +9,16 @@ from django.urls import reverse
 class Event(models.Model):
   class EventType(models.TextChoices):
     TRIP = 'Trip'
-    ACTIVITY = 'ACTIVITY'
+    ACTIVITY = 'Activity'
   name = models.CharField(max_length=150)
-  startDate = models.DateField('Start Date', default=timezone.now)
-  endDate = models.DateField('End Date', default=timezone.now() + timezone.timedelta(days=1))
-  location = models.CharField(max_length=250)
+  startDate = models.DateField('Start Date', default=str(timezone.now))
+  endDate = models.DateField('End Date', default= str(timezone.now() + timezone.timedelta(days=1)))
+  location = models.CharField(max_length=250, default='')
   description = models.TextField
   event_type = models.CharField(
     max_length=20,
     choices=EventType.choices,
+    default=EventType.TRIP,
   )
   # need to add the poll embed
   # need to add the profile ref
@@ -26,7 +27,7 @@ class Trips(models.Model):
   # travelers = ref profile
   event = models.OneToOneField(Event, on_delete=models.CASCADE)
   def duration(self):
-    if self.event.eventType == 'Trip':
+    if self.event.event_type == 'Trip':
       return (self.event.endDate - self.event.startDate).days
     else:
       return None
