e796241ebe259345bcf6218d053c621f25d82cda
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-12 15:40:56 -0500

    update url files to allow appwrite integration

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import test_backend
+from .views import test_backend, get_user_info
 
 # urlpatterns = [
 #     path('hello-world', views.hello_world, name='hello_world')
@@ -8,6 +8,7 @@ from .views import test_backend
 
 urlpatterns = [
     path('test-backend/', test_backend, name='test_backend'),
+    path('api/get_user_info/', get_user_info, name='get_user_info'),
     # path('dashboard/', views.dashboard, name='dashboard')
 ]
 
